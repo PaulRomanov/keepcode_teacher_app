b135f4949782b103b1a001bfc88013829b41380a
pavel.romanov <pavel.ramanau@gmail.com>
2025-08-22 13:24:26 +0200

    Setup basic project structure and styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="app-layout">
+    <aside class="app-layout__sidebar">
+    </aside>
+    <div class="app-layout__content-wrapper">
+      <header class="app-layout__header">
+      </header>
+      <main class="app-layout__main-content">
+      </main>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+
+</script>
+
+<style lang="scss">
+.app-layout {
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+
+  &__sidebar {
+    width: 138px;
+    flex-shrink: 0;
+    background-color: $color-bg-blue;
+  }
+
+  &__content-wrapper {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    overflow-y: auto;
+  }
+
+  &__header {
+    height: 72px;
+    flex-shrink: 0;
+    background-color: $color-bg-white;
+  }
+
+  &__main-content {
+    padding: 24px;
+    flex-grow: 1;
+  }
+}
+</style>
