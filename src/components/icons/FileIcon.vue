47d55b8a488d762652f7a0b72484507703fbf317
pavel.romanov <pavel.ramanau@gmail.com>
2025-08-24 14:52:48 +0200

    Fixed errors when building the project

--- src/components/icons/FileIcon.vue
+++ src/components/icons/FileIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="file-icon-wrapper">
     <PDFIcon v-if="fileType === 'pdf'" class="file-icon" />
-    <JpgIcon v-else-if="fileType === 'jpg'" class="file-icon" />
+    <JPGIcon v-else-if="fileType === 'jpg'" class="file-icon" />
     <DocIcon v-else-if="fileType === 'doc'" class="file-icon" />
     <XlsIcon v-else-if="fileType === 'xls'" class="file-icon" />
     <PDFIcon v-else class="file-icon" />
@@ -11,7 +11,7 @@
 <script setup>
   import { defineProps } from 'vue';
   import PDFIcon from './PDFIcon.vue';
-  import JpgIcon from './JpgIcon.vue';
+  import JPGIcon from './JPGIcon.vue';
   import DocIcon from './DocIcon.vue';
   import XlsIcon from './XlsIcon.vue';
 
